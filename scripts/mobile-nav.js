7b351e7ae3049b670e7cf67498a9946d5f83b34f
sykhdair1-ai <sykhdair1@students.everettcc.edu>
2026-06-16 17:11:23 -0700

    updated files

---
+++ scripts/mobile-nav.js
@@ -0,0 +1,13 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+if (toggleButton && navbarLinks) {
+
+toggleButton.addEventListener('click', () => {
+
+navbarLinks.classList.toggle('active');
+
+ });
+
+ }
